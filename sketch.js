b6b203e3e82628b34e9547570228a575050e732f
aditi716 <adidiv309@gmail.com>
2020-11-29 12:36:43 +0530

    car racing game 1.5

--- sketch.js
+++ sketch.js
@@ -9,8 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1IMG, car2IMG, car3IMG, car4IMG, ground, track;
 
-
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1IMG = loadImage("../images/car1.png");
+  car2IMG = loadImage("../images/car2.png");
+  car3IMG = loadImage("../images/car3.png");
+  car4IMG = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
